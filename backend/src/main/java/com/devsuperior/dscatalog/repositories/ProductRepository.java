b7563f5dad49c85e06f8aa0d3f7e1a9af205c187
Ricardo Lazaro <ricardoalazaro@hotmail.com>
2021-07-12 19:21:00 -0300

    Fix: Including categories on Product return and fix N+1 queries

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
@@ -16,6 +16,9 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	@Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE "
 			+ "(COALESCE(:categories) IS NULL OR cats IN :categories) AND "
-			+ "(:name = '' OR LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%')))") // relação Produto x Categoria Muitos para Muitos
+			+ "(LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%')))") // relação Produto x Categoria Muitos para Muitos
 	Page<Product> find(List<Category> categories, String name, Pageable pageable);
+	
+	@Query("SELECT obj FROM Product obj JOIN FETCH obj.categories WHERE obj IN :products") // para evitar N+1 consultas
+	List<Product> findProductsWithCategories (List<Product> products);
 }
